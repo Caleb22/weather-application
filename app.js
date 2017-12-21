28d694dd65f6e950d25ace2f0333dbbadafe59d6
micheal <micheal4s@live.co.uk>
2017-12-21 18:43:31 +0000

    Weather Forecast App

---
+++ app.js
@@ -0,0 +1,49 @@
+var http = require('http');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+//static lockfiles
+app.use(express.static('./public'));
+
+  http.get('http://api.openweathermap.org/data/2.5/forecast?q=London,GB&appid=c4bce5bf5e664247e5751880f1b9c19d&units=metric', function(response){
+    //console.log(response.statusCode);
+    var info = "";
+    response.on("data",function(chunk){
+     info += chunk;
+    });
+
+    response.on("end",function(){
+      if(response.statusCode === 200){
+        //console.log(typeof(data));
+        try{
+          //parsing the string into JSON object
+         app.get('/', function(req,res){
+           var data = JSON.parse(info);
+          //data.list[0].main;
+           var datas = {temp: data.list[0].main.temp, humidity: data.list[0].main.humidity,
+              dt_txt: data.list[0].dt_txt, pressure: data.list[0].main.pressure,
+              temp2: data.list[2].main.temp, humidity2: data.list[2].main.humidity,
+              dt_txt2: data.list[2].dt_txt, pressure2: data.list[2].main.pressure,
+              temp3: data.list[10].main.temp, humidity3: data.list[10].main.humidity,
+              dt_txt3: data.list[10].dt_txt, pressure3: data.list[10].main.pressure,
+              temp4: data.list[19].main.temp, humidity4: data.list[19].main.humidity,
+              dt_txt4: data.list[19].dt_txt, pressure4: data.list[19].main.pressure,
+              temp5: data.list[28].main.temp, humidity5: data.list[28].main.humidity,
+              dt_txt5: data.list[28].dt_txt, pressure5: data.list[28].main.pressure};
+              //console.log(datas);
+          res.render('index', {datas: datas});
+           });
+        }catch(error){
+          console.log("sorry wrong try");
+        }
+      }else{
+        console.log('sorry something wrong');
+      }
+    });
+  });
+
+    //listen to port
+  app.listen(3000);
